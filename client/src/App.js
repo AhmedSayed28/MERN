cc68c254175c6d6c9c7656bfea3f064f47f4e066
AhmedSayed14 <ahm.say14@gmail.com>
2023-06-05 10:29:57 +0300

    code best practise for clientSide  with  css style

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React, { useState , useEffect } from 'react'
 import Axios from 'axios'
+import'./App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container , Button , Badge , ListGroup , Form, ListGroupItem } from 'react-bootstrap'
 
 function App() {
+
+  const api = "http://localhost:3001";
 
   const [users , setUsers] = useState([])
   const [name , setName] = useState("")
@@ -9,49 +14,73 @@ function App() {
   const [email , setEmail] = useState("")
 
   useEffect(() => { 
-    Axios.get("http://localhost:3000/users").then((response) => {
-      setUsers(response.data)
-    })
+    Axios.get(`${api}/users`).then((response) =>setUsers(response.data))
   }, [users]); // [] => بتشتغل لما الصفحة تحمل بس مره واحده
-  // [users] => بتشتغل لما الصفحة تحمل ولما ال users يتغير
+              // [users] => بتشتغل لما الصفحة تحمل ولما ال users يتغير
 
 
-  const AddUser = (e) => {
-    Axios.post("http://localhost:3000/addUser", {
-      name:name,
-      age:age,
-      email:email,
-    })
-    .then(res => {
-      console.log(res.data)
-    })
-    e.preventDefault();
+  const AddUser = () => {
+    if(name && age && email){
+      Axios.post(`${api}/addUser`, {name,age,email})
+     .then(res => res.data)
+    }else{
+      alert("Please fill all fields")
+    }
   }
 
   return (
-    <>
-    {
-      users.map((user, index) => {
-        return <div className="App" key={index}>
-          <ul>
-            <li>name : {user.name}</li>
-            <li>Age : {user.age}</li>
-            <li>Email : {user.email}</li>
-          </ul>
-        </div>
+
+    <Container>
+      <Form className='form'>
+        <Form.Control type="text" placeholder="name" onChange={event=>setName(event.target.value)}/>
+        <Form.Control type="number" placeholder="Age" onChange={event=>setAge(event.target.value)}/>
+        <Form.Control type="text" placeholder="email" onChange={event=>setEmail(event.target.value)}/>
+        <Button className='btn btn-success' onClick={AddUser}>Add User</Button>
+      </Form>
+
+    <div className='result'>
+      {
+      users.map(({_id , name , age , email}) => {
+        return (
+          <ListGroup key={_id}>
+            <ListGroupItem variant='dark' className='text-center'>
+              <div className='ms-2 me-auto'>
+                <div className='fw-bold'>Name : {name}</div>
+                <div>Email : {email}</div>
+              </div>
+              Age : <Badge bg='success' pill>{age}</Badge>
+            </ListGroupItem>
+          </ListGroup>
+        )
       })
     }
+    </div>
+    </Container>
 
 
-    <form>
-      <input type="text" placeholder="name" onChange={event=>setName(event.target.value)}/>
-      <input type="number" placeholder="age" onChange={event=>setAge(event.target.value)}/>
-      <input type="email" placeholder="email" onChange={event=>setEmail(event.target.value)}/>
-      <button onClick={AddUser}>Add User</button>
-    </form>
+    // <>
+    // {
+    //   users.map(({_id , name , age , email}) => {
+    //     return <div className="App" key={_id}>
+    //       <ul>
+    //         <li>name : {name}</li>
+    //         <li>Age : {age}</li>
+    //         <li>Email : {email}</li>
+    //       </ul>
+    //     </div>
+    //   })
+    // }
 
 
-    </>
+    // <form>
+    //   <input type="text" placeholder="name" onChange={event=>setName(event.target.value)}/>
+    //   <input type="number" placeholder="age" onChange={event=>setAge(event.target.value)}/>
+    //   <input type="email" placeholder="email" onChange={event=>setEmail(event.target.value)}/>
+    //   <button onClick={AddUser}>Add User</button>
+    // </form>
+
+
+    // </>
   )
 }
 
